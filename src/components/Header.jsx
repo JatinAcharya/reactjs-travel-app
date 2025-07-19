b164a58c4576df57e4d55059cf6e27cf36ba8fed
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-19 15:37:17 +0530

    Header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="custom-header">
+        <img src="/src/assets/globe.png" alt="Globe icon" />
+        <h2>My Travel Journal</h2>
+      </header>
+    </>
+  );
+};
+
+export default Header;
